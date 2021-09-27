b80dd7eecebf3beb3f84950dda80e572eebd0de1
AHTOH2001 <antonlapytko@gmail.com>
2021-09-27 08:57:38 +0300

    Before 4.1.3

---
+++ Igi_project/Areas/Admin/Pages/Details.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Igi_project.Data;
+using Igi_project.Entities;
+
+namespace Igi_project
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Igi_project.Data.ApplicationDbContext _context;
+
+        public DetailsModel(Igi_project.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Dish Dish { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Dish = await _context.Dishes
+                .Include(d => d.Group).FirstOrDefaultAsync(m => m.DishId == id);
+
+            if (Dish == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ Igi_project/Areas/Admin/Pages/Edit.cshtml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Igi_project.Data;
+using Igi_project.Entities;
+
+namespace Igi_project
+{
+    public class EditModel : PageModel
+    {
+        private readonly Igi_project.Data.ApplicationDbContext _context;
+
+        public EditModel(Igi_project.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Dish Dish { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Dish = await _context.Dishes
+                .Include(d => d.Group).FirstOrDefaultAsync(m => m.DishId == id);
+
+            if (Dish == null)
+            {
+                return NotFound();
+            }
+           ViewData["DishGroupId"] = new SelectList(_context.DishGroups, "DishGroupId", "DishGroupId");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Dish).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DishExists(Dish.DishId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool DishExists(int id)
+        {
+            return _context.Dishes.Any(e => e.DishId == id);
+        }
+    }
+}
